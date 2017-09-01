f2f915b799afa1b4b80b0596e81282898bc163e0
ethanwalters <ethanwalters@192.168.0.39>
2017-09-01 10:04:14 -0600

    all done boi

--- While Loop Challenges/src/WhileLoopChallenges.java
+++ While Loop Challenges/src/WhileLoopChallenges.java
@@ -5,59 +5,59 @@ public class WhileLoopChallenges
 	{
 		public static void main(String[] args)
 			{
-				
-				//challenge one
-				
+
+				// challenge one
+
 				int i = 5;
-				while( i > 0){
-					System.out.println(i);
-					
-					i = i - 1;
-					
-				}
-				
-				//challenge two
-				
-				boolean thierLetterB = true;
-				
-				do{
-					Scanner userInput = new Scanner(System.in);
-					System.out.println("Enter your favorite letter");
-					String thierLetter = userInput.nextLine();
-					
-					if(thierLetter.equals("q")){
-						thierLetterB = false;
+				while (i > 0)
+					{
+						System.out.println(i);
+
+						i = i - 1;
+
 					}
-					
-				}
-				while(thierLetterB);
-				
+
+				// challenge two
+
+				boolean thierLetterB = true;
+
+				do
+					{
+						Scanner userInput = new Scanner(System.in);
+						System.out.println("Enter your favorite letter");
+						String thierLetter = userInput.nextLine();
+
+						if (thierLetter.equals("q"))
+							{
+								thierLetterB = false;
+							}
+
+					} while (thierLetterB);
+
 				System.out.println("q is now your new favorite letter! \n");
-				
-				
+
 				// challenge three
 				boolean lit = true;
-				
+
 				Random rad = new Random();
-				
+
 				int randomNumber = rad.nextInt(10) + 1;
-				
-				do{
-					Scanner favLetter = new Scanner(System.in);
-					System.out.println("Guess my favorite number! \n(hint! its between 0-10)");
-					
-					int letter = favLetter.nextInt();
-					
-					
-					if(letter == randomNumber){
-						lit = false;
-					}
-					
-				}
-				while(lit);
-				
+
+				do
+					{
+						Scanner favLetter = new Scanner(System.in);
+						System.out.println("Guess my favorite number! \n(hint! its between 0-10)");
+
+						int letter = favLetter.nextInt();
+
+						if (letter == randomNumber)
+							{
+								lit = false;
+							}
+
+					} while (lit);
+
 				System.out.println("\nYou have guessed my favorite number! \nThanks for playing!");
-				
 
 			}
 	}
